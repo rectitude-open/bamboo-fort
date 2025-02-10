e76755f524a86e2bcec82c5691f3196022ee5769
aspirantzhang <admin@aspirantzhang.com>
2025-02-10 15:55:30 +0800

    fix: update site URL in docusaurus configuration

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://bamboo-fort.rectitude.cc',
+  url: 'https://bamboofort.rectitude.cc',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
